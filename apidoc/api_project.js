75959ad5e57768bdd1f7b093ef45f499573965d3
左盐 <huabinglan@163.com>
2016-04-02 17:58:43 +0800

    添加api文档文件

---
+++ apidoc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "renmaiApi",
+  "version": "1.0.0",
+  "description": "renmai Api power by golang neo",
+  "title": "pizzaCmsApi",
+  "url": "http://192.168.1.134:3004/v1",
+  "sampleUrl": "http://192.168.1.134:3004/v1",
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-04-01T10:52:51.695Z",
+    "url": "http://apidocjs.com",
+    "version": "0.15.1"
+  }
+});
